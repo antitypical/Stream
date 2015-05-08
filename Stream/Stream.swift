f69e9ec450c832c3492ae7886dc4c53e578ddc31
Rob Rix <rob.rix@github.com>
2015-05-08 18:01:04 -0400

    Stream is NilLiteralConvertible.

--- Stream/Stream.swift
+++ Stream/Stream.swift
@@ -1,13 +1,23 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
 /// An iterable stream.
-public enum Stream<T> {
+public enum Stream<T>: NilLiteralConvertible {
+	// MARK: NilLiteralConvertible
+
+	/// Constructs a `Nil` `Stream`.
+	public init(nilLiteral: ()) {
+		self = Nil
+	}
+
+
 	// MARK: Cases
 
 	/// A `Stream` of a `T` and the lazily memoized rest of the `Stream`.
 	case Cons(Box<T>, Memo<Stream<T>>)
 
 	/// The empty `Stream`.
+	///
+	/// Avoid using this directly; instead, use `nil`: `Stream` conforms to `NilLiteralConvertible`, and `nil` has better properties with respect to type inferencing.
 	case Nil
 }
 
