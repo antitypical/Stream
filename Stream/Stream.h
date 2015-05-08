ccbdf00d9d092b18b5363ad0bd416baf335fda37
Rob Rix <rob.rix@github.com>
2015-05-08 17:40:08 -0400

    Initial Commit

---
+++ Stream/Stream.h
@@ -0,0 +1,8 @@
+//  Copyright (c) 2015 Rob Rix. All rights reserved.
+
+/// Project version number for Stream.
+extern double StreamVersionNumber;
+
+/// Project version string for Stream.
+extern const unsigned char StreamVersionString[];
+
---
+++ StreamTests/StreamTests.swift
@@ -0,0 +1,5 @@
+//  Copyright (c) 2015 Rob Rix. All rights reserved.
+
+import XCTest
+
+final class StreamTests: XCTestCase {}
